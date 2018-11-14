08a7d73e5048cdaf68889433ccb0e07c26f2220c
Jonathan Borg <jonathan@x3m.io>
2018-11-14 03:07:26 +0100

    added prettier integration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import { extname, dirname, parse as parseFilename } from 'path';
-import { readFileSync } from 'fs';
-import { parse } from '@babel/parser';
-import { declare } from '@babel/helper-plugin-utils';
-import resolve from 'resolve';
+import { extname, dirname, parse as parseFilename } from "path";
+import { readFileSync } from "fs";
+import { parse } from "@babel/parser";
+import { declare } from "@babel/helper-plugin-utils";
+import resolve from "resolve";
 
-import optimize from './optimize';
-import escapeBraces from './escapeBraces';
-import transformSvg from './transformSvg';
-import fileExistsWithCaseSync from './fileExistsWithCaseSync';
-import { hyphenToCamel } from './camelize';
-import capitalize from './capitalize';
+import optimize from "./optimize";
+import escapeBraces from "./escapeBraces";
+import transformSvg from "./transformSvg";
+import fileExistsWithCaseSync from "./fileExistsWithCaseSync";
+import { hyphenToCamel } from "./camelize";
+import capitalize from "./capitalize";
 
 let ignoreRegex;
 
-export default declare(({
- assertVersion, template, traverse, types: t 
-}) => {
+export default declare(({ assertVersion, template, traverse, types: t }) => {
   assertVersion(7);
 
   const buildSvg = template(`
@@ -42,15 +40,15 @@ export default declare(({
     importPath,
     path,
     state,
-    defaultExport,
+    defaultExport
   ) {
-    if (typeof importPath !== 'string') {
-      throw new TypeError('`applyPlugin` `importPath` must be a string');
+    if (typeof importPath !== "string") {
+      throw new TypeError("`applyPlugin` `importPath` must be a string");
     }
     const {
       ignorePattern,
       caseSensitive,
-      filename: providedFilename,
+      filename: providedFilename
     } = state.opts;
     const { file, filename } = state;
     if (ignorePattern) {
@@ -62,38 +60,39 @@ export default declare(({
       }
     }
     // This plugin only applies for SVGs:
-    if (extname(importPath) === '.svg') {
+    if (extname(importPath) === ".svg") {
       const iconPath = filename || providedFilename;
       const svgPath = resolve.sync(importPath, { basedir: dirname(iconPath) });
       if (caseSensitive && !fileExistsWithCaseSync(svgPath)) {
         throw new Error(
-          `File path didn't match case of file on disk: ${svgPath}`,
+          `File path didn't match case of file on disk: ${svgPath}`
         );
       }
       if (!svgPath) {
         throw new Error(`File path does not exist: ${importPath}`);
       }
-      const rawSource = readFileSync(svgPath, 'utf8');
-      const optimizedSource =        state.opts.svgo === false
+      const rawSource = readFileSync(svgPath, "utf8");
+      const optimizedSource =
+        state.opts.svgo === false
           ? rawSource
           : optimize(rawSource, state.opts.svgo);
 
       const escapeSvgSource = escapeBraces(optimizedSource);
 
       const parsedSvgAst = parse(escapeSvgSource, {
-        sourceType: 'module',
-        plugins: ['jsx'],
+        sourceType: "module",
+        plugins: ["jsx"]
       });
 
       traverse(parsedSvgAst, transformSvg(t));
 
       const svgCode = traverse.removeProperties(
-        parsedSvgAst.program.body[0].expression,
+        parsedSvgAst.program.body[0].expression
       );
 
       const opts = {
         SVG_NAME: importIdentifier,
-        SVG_CODE: svgCode,
+        SVG_CODE: svgCode
       };
 
       // Move props off of element and into defaultProps
@@ -101,12 +100,12 @@ export default declare(({
         const keepProps = [];
         const defaultProps = [];
 
-        svgCode.openingElement.attributes.forEach((prop) => {
-          if (prop.type === 'JSXSpreadAttribute') {
+        svgCode.openingElement.attributes.forEach(prop => {
+          if (prop.type === "JSXSpreadAttribute") {
             keepProps.push(prop);
           } else {
             defaultProps.push(
-              t.objectProperty(t.identifier(prop.name.name), prop.value),
+              t.objectProperty(t.identifier(prop.name.name), prop.value)
             );
           }
         });
@@ -124,8 +123,8 @@ export default declare(({
         const svgReplacement = defaultExport ? exportSVG(opts) : buildSvg(opts);
         path.replaceWith(svgReplacement);
       }
-      file.get('ensureReact')();
-      file.set('ensureReact', () => {});
+      file.get("ensureReact")();
+      file.set("ensureReact", () => {});
     }
   }
 
@@ -134,34 +133,34 @@ export default declare(({
       Program: {
         enter({ scope, node }, { file, opts, filename }) {
           if (
-            typeof filename === 'string'
-            && typeof opts.filename !== 'undefined'
+            typeof filename === "string" &&
+            typeof opts.filename !== "undefined"
           ) {
             throw new TypeError(
-              'the "filename" option may only be provided when transforming code',
+              'the "filename" option may only be provided when transforming code'
             );
           }
           if (
-            typeof filename === 'undefined'
-            && typeof opts.filename !== 'string'
+            typeof filename === "undefined" &&
+            typeof opts.filename !== "string"
           ) {
             throw new TypeError(
-              'the "filename" option is required when transforming code',
+              'the "filename" option is required when transforming code'
             );
           }
-          if (!scope.hasBinding('React')) {
+          if (!scope.hasBinding("React")) {
             const reactImportDeclaration = t.importDeclaration(
-              [t.importDefaultSpecifier(t.identifier('React'))],
-              t.stringLiteral('react'),
+              [t.importDefaultSpecifier(t.identifier("React"))],
+              t.stringLiteral("react")
             );
 
-            file.set('ensureReact', () => {
+            file.set("ensureReact", () => {
               node.body.unshift(reactImportDeclaration);
             });
           } else {
-            file.set('ensureReact', () => {});
+            file.set("ensureReact", () => {});
           }
-        },
+        }
       },
       CallExpression(path, state) {
         const { node } = path;
@@ -170,15 +169,15 @@ export default declare(({
           ? requireArg.value
           : null;
         if (
-          node.callee.name === 'require'
-          && t.isVariableDeclarator(path.parent)
-          && filePath
+          node.callee.name === "require" &&
+          t.isVariableDeclarator(path.parent) &&
+          filePath
         ) {
           applyPlugin(
             path.parent.id,
             filePath,
             path.parentPath.parentPath,
-            state,
+            state
           );
         }
       },
@@ -191,17 +190,18 @@ export default declare(({
       ExportNamedDeclaration(path, state) {
         const { node } = path;
         if (
-          node.specifiers.length > 0
-          && node.specifiers[0].local.name === 'default'
+          node.specifiers.length > 0 &&
+          node.specifiers[0].local.name === "default"
         ) {
           const exportName = node.specifiers[0].exported.name;
           const filename = parseFilename(node.source.value).name;
-          const componentName = exportName === 'default'
+          const componentName =
+            exportName === "default"
               ? capitalize(hyphenToCamel(filename))
               : exportName;
           applyPlugin(componentName, node.source.value, path, state, true);
         }
-      },
-    },
+      }
+    }
   };
 });
